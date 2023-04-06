42dabebf0e5fb9cbff68dfe0754b75e3040c861e
sprtd <sagesdn@gmail.com>
2023-04-06 13:27:38 +0100

    test: change number

--- test/Storage.js
+++ test/Storage.js
@@ -6,6 +6,8 @@ const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
 const { expect } = require("chai");
 
 describe("Storage Test Suite", async () => {
+    const { parseEther, formatEther } = await ethers.utils;
+
     let owner, addr1, addr2, storageContract
 
     beforeEach(async () => {
@@ -21,6 +23,20 @@ describe("Storage Test Suite", async () => {
             expect(await storageContract.owner()).to.equal(owner.address);
             expect(await storageContract.owner()).to.not.equal(addr1.address)
             expect(await storageContract.owner()).to.not.equal(addr2.address)
+        });
+
+        it("should change number", async function () {
+            // const value1 = await parseEther("100")
+            const value1 = 100
+            // const changeNumberTxn = await storageContract.changeNumber(value1)
+            await storageContract.changeNumber(value1)
+            // await changeNumberTxn.wait()
+
+            const number = await storageContract.number()
+            // console.log("stored number__", formatEther(value1))
+            expect(number).to.eq(value1)
+            expect(number).to.not.eq(0)
+
         });
     })
 
